fd81806cf4d2e7529e6c13cb38d7430a53aafea2
FelipeFavato <felipelopesfavato@gmail.com>
2024-04-02 11:29:16 -0300

    Javascript e java finalizados

---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+  // const string1 = word1.join('');
+  // const string2 = word2.join('');
+
+  // for (let i = 0; i < string1.length; i++) {
+  //   if (string1[i] === ',') {
+  //     console.log(string1[i]);
+  //     string1[i] = '';
+  //     console.log(string1[i]);
+  //   }
+  // }
+
+  // for (let subS2 of string2) {
+  //   if (subS2 === ',') {
+  //     subS2.replace(subS2, '');
+  //   }
+  // }
+
+  // console.log(string1);
+  // console.log(string2);
